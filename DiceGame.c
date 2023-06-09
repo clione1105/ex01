77785aaff081a45aa4b233d41b60a2e19cca64b7
SATO Takumi <avaperu1105@gmail.com>
2023-06-09 11:54:13 +0900

    Dice game: initial revision

---
+++ DiceGame.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  int d[2], total = 0;
+  srand((unsigned int)time(NULL));
+  
+  printf("Rolling dice...\n");
+
+  for (int i = 0; i < 2; i++)
+    {
+      d[i] = rand() % 6 + 1;
+      total += d[i];
+      printf("Die %d: %d\n", i + 1, d[i]);
+    }
+
+  printf("Total value: %d\n", total);
+  
+}
